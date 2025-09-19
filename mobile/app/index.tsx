228886906cd6552271e42fe4df8d63cc37afa860
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-09-19 16:35:36 +0100

    fixed error

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -42,7 +42,7 @@ export default function Index() {
       setMessages((prev) => [...prev, geminiMessage]);
       setReply(null);
     }
-  }, [reply]);
+  }, [reply, setReply]);
 
   useEffect(() => {
     if (flatListRef.current) {

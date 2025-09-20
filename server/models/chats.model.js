15cfc66c7d35dbd9c1d4bdfb38ff540a1ff5a566
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-09-20 07:03:20 +0100

    Added needed models

---
+++ server/models/chats.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const ChatSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Chat", ChatSchema);

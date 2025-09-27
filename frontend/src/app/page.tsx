9b5d9fe7d15ab96d218b0ecb33607f988199e45e
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-09-27 02:35:39 +0100

    Added school badges component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import SchoolBadges from "@/components/SchoolBadges";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <SchoolBadges />
     </div>
   );
 }

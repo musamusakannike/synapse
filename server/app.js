2ce942d4df5b9ff6da12b448b98ed5f7daf2bed6
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-09-28 14:29:41 +0100

    Added quiz routes

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const connectDB = require("./config/db.config");
 const authRoutes = require("./routes/auth.route");
 const chatRoutes = require("./routes/chat.route");
 const documentRoutes = require("./routes/document.route");
+const quizRoutes = require("./routes/quiz.route");
 
 const app = express();
 connectDB();
@@ -34,5 +35,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/chats", chatRoutes);
 app.use("/api/documents", documentRoutes);
+app.use("/api/quizzes", quizRoutes);
 
 module.exports = app;

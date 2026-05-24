28989ba312569ed8c581d037d5bc45825aaf8b84
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2026-05-24 21:24:16 +0100

    removed whole project

--- server/routes/tts.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { generateTTS } = require("../controllers/tts.controller");
-const { authenticate } = require("../middlewares/auth.middleware");
-const router = express.Router();
-
-router.post("/", authenticate, generateTTS);
-
-module.exports = router;
